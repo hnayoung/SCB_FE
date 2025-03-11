759234a00be375e6c6864bf6f610c4621adbe998
geunpyoPark <rmse303@gmail.com>
2025-03-11 23:00:36 +0900

    랭킹 완료 및 게시판 얼추 완성

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import NotFound from "./pages/NotFound"; // 404 페이지 가져오기
 import MainLayout from "./pages/components/Mainlayout";
 import MyPage from "./pages/mypage/MyPage"; // 마이페이지 컴포넌트 가져오기
 import Ranking from "./pages/ranking/Ranking";
+import Board from "./pages/board/Board";
 
 function App() {
   return (
@@ -16,7 +17,8 @@ function App() {
         <Route path="/signup" element={<SignupForm />} />
         <Route path="/community/:communityName" element={<Community />} /> {/* 커뮤니티 페이지 */}
         <Route path="/mypage" element={<MyPage />} /> {/* 마이페이지 경로 추가 */}
-        <Route path="/ranking" element={<Ranking />} /> {/* 마이페이지 경로 추가 */}
+        <Route path="/ranking" element={<Ranking />} /> {/* 랭킹페이지 경로 추가 */}
+        <Route path="/board" element={<Board />} /> {/*랭킹페이지 경로 추가*/}
         <Route path="/" element={<MainLayout />} />
         <Route path="*" element={<NotFound />} /> {/* 잘못된 경로에 대해 404 페이지 */}
       </Routes>
